1eb0f38fe33e831bd13361d269032eab4502c67b
Erika Perez <erikaperez@Erikas-MacBook-Air.local>
2019-07-29 20:56:14 -0400

    onchange on input, completeted

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 let state = {
     start: false,
     todos: [],
+    newTodo: '',
 }
 
 
@@ -10,3 +11,12 @@ let state = {
 const addTodoButton = document.querySelector('.js-add-button');
 const inputField = document.querySelector(".js-input");
 const listContainer = document.querySelector(".js-list");
+
+//------Events--------------
+
+inputField.addEventListener('input', (e) => {
+    if (e.data) {
+        state.newTodo += e.data;
+        console.log(state.newTodo)
+    }
+})
