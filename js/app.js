30ba6fc25381a671ca2c230dd760ba606546c980
Erika Perez <erikaperez@Erikas-MacBook-Air.local>
2019-07-29 20:09:01 -0400

    added HTML object in js file

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ let state = {
     start: false,
     todos: [],
 }
+
+
+//-----HTML Objects---------
+
+const addTodoButton = document.querySelector('.js-add-button');
+const inputField = document.querySelector(".js-input");
